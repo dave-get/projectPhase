3f355d61a643dcc4ceb53dbb121d4b58391f6b5b
Your GitHub Username <your-email@example.com>
2024-08-21 02:52:01 -0900

    welcome page added

--- Final Task/app/authPages/loginPage/page.tsx
+++ Final Task/app/authPages/loginPage/page.tsx
@@ -7,7 +7,7 @@ const SignIn = () => {
   return (
     <>
       <div className="contain bg-slate-100 min-h-svh md:h-fullScreen flex flex-col md:flex-row md:p-16 p-5 gap-5">
-        <div className="pageinfo border-slate-300 border h-full md:w-6/12">
+        <div className="pageinfo border-slate-300 h-full md:w-6/12">
           <Image
             src="/welcome.jpg"
             alt=""
@@ -16,7 +16,7 @@ const SignIn = () => {
             height={1000}
           />
         </div>
-        <div className="bg-slate-200 flex flex-col pt-10 gap-5 md:w-5/12 border-slate-300 border px-5 h-full">
+        <div className="flex flex-col bg-white pt-10 gap-5 md:w-5/12 px-5 h-full">
           <h1 className="font-bold text-large border-b w-full text-center font-poppins pb-5">
             Welcome Back
           </h1>
--- Final Task/app/page.tsx
+++ Final Task/app/page.tsx
@@ -1,5 +1,27 @@
-import SignIn from "./authPages/loginPage/page";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <SignIn /> 
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen bg-slate-200 relative">
+      <div className="flex fixed top-0 justify-between px-32 py-5 items-center bg-slate-200 w-full border-b shadow-lg">
+        <Image
+          src="/akil-logo.png"
+          width={1000}
+          height={1000}
+          className="w-20 h-6"
+          alt=""
+        />
+        <p className="text-customeDark font-epilogue font-bold text-header2">
+          Welcome To AKIL
+        </p>
+      </div>
+      <Link
+        href="/authPages/loginPage"
+        className="border border-slate-300 px-5 py-3 w-fit hover:bg-slate-100 hover:shadow-[0_10px_20px_10px_rgba(210,200,200,0.5)] rounded-3xl text-ellipsis font-extrabold text-customeDark"
+      >
+        GET START
+      </Link>
+    </div>
+  );
 }
